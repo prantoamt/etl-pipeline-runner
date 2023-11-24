8bda0fd4f0a4cadb969b16e3877c924469b9f673
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2023-11-24 10:08:58 +0100

    nox file added

---
+++ noxfile.py
@@ -0,0 +1,14 @@
+import os
+import nox
+
+os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
+
+@nox.session
+def tests(session):
+    session.run_always('pdm', 'install', '-G', 'test', external=True)
+    session.run('pytest')
+
+@nox.session
+def lint(session):
+    session.run_always('pdm', 'install', '-G', 'lint', external=True)
+    session.run('flake8', '--import-order-style', 'google')
